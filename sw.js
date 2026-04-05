1969a90411f1d8eff78a0c02aaff2b49aedc7fc0
mendozamarco138-collab <mendozamarco138@gmail.com>
2026-04-05 19:49:13 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('marco-inventory-v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './Inventory.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
